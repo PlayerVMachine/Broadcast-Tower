fc05dfc3aba9c8ac83e048be214f2debc6594f51
Nicolas <lehmannic22@gmail.com>
2018-04-17 21:09:59 -0400

    various commands added

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ const qeval = bot.registerCommand("qeval", async (msg, args) => {
 	}
 });
 
+const eval = bot.registerCommand("eval", (msg, args) => {
+	if (msg.author.id === config.creator) {
+		try {
+			eval(args.join(" "));
+		} catch (error) {
+			return "```\n The promise was rejected: " + error + "\n```";
+		}
+
+	} else {
+		return "Unathorized! :angry:"
+	}
+});
+
 //Create User
 const create = bot.registerCommand("create", async (msg, args) => {
 	let check = await db.get("User", msg.author.id, "Users"); //Check if user exists
@@ -65,14 +78,14 @@ edit.registerSubcommand ("tagline", async (msg, args) => {
 	try {
 		let tagline = await db.get("Tagline", msg.author.id, "Users");
 
-		bot.createMessage(msg.channel.id, "Your tagline is: " + JSON.stringify(tagline));
+		bot.createMessage(msg.channel.id, "Your tagline is: " + tagline[0].Tagline);
 		bot.createMessage(msg.channel.id, "Enter a new tagline:");
 
 		bot.on('messageCreate', async (newmsg) => {
 			if (newmsg.author.id === msg.author.id) {
  				let res = await db.set("Tagline", newmsg.author.id, "Users", newmsg.content);
  				if (res === 1)
- 					bot.createMessage(msg.channel.id, "Tagline updated successfully");
+ 					bot.createMessage(msg.channel.id, "Tagline updated successfully: " + newmsg.content);
  				else
  					bot.createMessage(msg.channel.id, "An error occured setting your tagline please try again.");
  			}
@@ -83,11 +96,68 @@ edit.registerSubcommand ("tagline", async (msg, args) => {
 	}
 }, {
 	description: "Edit your tagline",
-	fullDescription: "Edit your tagline"
+	fullDescription: "Edit your tagline, maximum 140 characters"
 });
 
-//Set Status
+edit.registerSubcommand ("profile", async (msg, args) => {
+	try {
+		let tagline = await db.get("Profile", msg.author.id, "Users");
 
+		bot.createMessage(msg.channel.id, "Your Profile is: " + tagline[0].Profile);
+		bot.createMessage(msg.channel.id, "Enter a new profile:");
+
+		bot.on('messageCreate', async (newmsg) => {
+			if (newmsg.author.id === msg.author.id) {
+ 				let res = await db.set("Profile", newmsg.author.id, "Users", newmsg.content);
+ 				if (res === 1)
+ 					bot.createMessage(msg.channel.id, "Profile updated successfully: " + newmsg.content);
+ 				else
+ 					bot.createMessage(msg.channel.id, "An error occured setting your profile please try again.");
+ 			}
+ 		});
+
+	} catch (e) {
+		console.log(e);
+	}
+}, {
+	description: "Edit your tagline",
+	fullDescription: "Edit your tagline, maximum 500 characters"
+});
+
+//Get Status, possible statuses: actvie, banned, disabled,
+edit.registerSubcommand ("status", async (msg,args) => {
+	try {
+		let status = await db.get("State", msg.author.id, "Users");
+
+		bot.createMessage(msg.channel.id, "Your account status is: " + status[0].State);
+
+		if (status[0].State === 'Banned')
+			bot.createMessage(msg.channel.id, "To appeal your ban head to <discord.gg/something> ");
+
+	} catch(e) {
+		console.log(e)
+	}
+}, {
+	description: "View your accout status",
+	fullDescription: "View your account status"
+});
+
+//follow a user
+const follow = registerCommand ("follow", async (msg, args) => {
+	try {
+		let following = await db.get("Following", msg.author.id, "Users");
+
+		list = parseJSON(following[0].Following);
+		list.indexOf(args[0])
+
+
+	} catch (e) {
+		console.log(e);
+	}
+}, {
+	description: "Follow a user",
+	fullDescription: "Follow a user, allows you to receive their posts"
+});
 
 //Send post
 
@@ -95,9 +165,11 @@ edit.registerSubcommand ("tagline", async (msg, args) => {
 //Delete post
 
 
-//Set post recieve location (default is DMs)
 
 
+function parseJSON(obj) {
+    return Function('"use strict";return (' + obj + ')')();
+}
 
 //start
 bot.on("ready", () => {
