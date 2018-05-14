72e492300430741788db351bbb4b30b0fc3a4bc9
Nicolas Lehman <lehmannic22@gmail.com>
2018-05-14 00:32:20 -0400

    test print

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ const post = bot.registerCommand('post', async (msg, args) => {
 
 
 const help = bot.registerCommand('help', (msg, args) => {
-	return bot.commands[0]
+	console.log(bot.commands[0])
 }, {
 	cooldown: 5000,
 	description: reply.help.description,
