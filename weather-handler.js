384a160187f31b6d92bc48c35781de5ad1c98dee
PlayerVM <lehmannic22@gamil.com>
2018-05-16 16:09:09 -0400

    weather with spaces

--- weather-handler.js
+++ weather-handler.js
@@ -2,40 +2,41 @@ const weather = require('weather-js');
 
 
 exports.getWeather = (msg, args, bot) => {
-
-  if (args.length === 0) {
-    bot.createMessage(msg.channel.id, 'please enter a location and degree type')
-    return
-  }
-
-  let command = args.join(' ')
-
-  if (command.split('-d')[0] === 'undefined') {
-    let location = command
-  } else {
-    let location = command.split('-d')[0].trim()
-  }
-  
-  if (command.split('-d')[1] === 'undefined') {
-    let degree = 'C'
-  } else {
-    let degree = command.split('-d')[1].trim()
-  }
-
-  weather.find({search: location, degreeType: args[1]}, (err, result) => {
-    if(err) {
-      console.log(err)
+  try {
+    if (args.length === 0) {
+      bot.createMessage(msg.channel.id, 'please enter a location and degree type')
       return
     }
 
-    let toSend = "**Current Weather in "
-    + result[0].location.name + "**\`\`\`Temperature: "
-    + result[0].current.temperature + result[0].location.degreetype
-    + "\nFeels like: " + result[0].current.feelslike
-    + result[0].location.degreetype + "\nSky: "
-    + result[0].current.skytext + "\nWind: "
-    + result[0].current.winddisplay + "\`\`\`";
+    let command = args.join(' ')
 
-    bot.createMessage(msg.channel.id, toSend)
-  })
+    let location = command
+    if (command.split('-d')[0] !== 'undefined') {
+      let location = command.split('-d')[0].trim()
+    }
+
+    let degree = 'C'
+    if (command.split('-d')[1] !== 'undefined') {
+      let degree = command.split('-d')[1].trim()
+    }
+
+    weather.find({search: location, degreeType: args[1]}, (err, result) => {
+      if(err) {
+        console.log(err)
+        return
+      }
+
+      let toSend = "**Current Weather in "
+      + result[0].location.name + "**\`\`\`Temperature: "
+      + result[0].current.temperature + result[0].location.degreetype
+      + "\nFeels like: " + result[0].current.feelslike
+      + result[0].location.degreetype + "\nSky: "
+      + result[0].current.skytext + "\nWind: "
+      + result[0].current.winddisplay + "\`\`\`";
+
+      bot.createMessage(msg.channel.id, toSend)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
