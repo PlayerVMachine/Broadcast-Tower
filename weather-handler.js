f301cb501135bbaafa6eb685ba9847ee5f6c9ff2
PlayerVM <lehmannic22@gamil.com>
2018-05-16 16:02:41 -0400

    weather with spaces

--- weather-handler.js
+++ weather-handler.js
@@ -6,23 +6,29 @@ exports.getWeather = (msg, args, bot) => {
   if (args.length === 0) {
     bot.createMessage(msg.channel.id, 'please enter a location and degree type')
     return
-  } else if (args[1].toUpperCase() === 'F' || args[1].toUpperCase() === 'C') {
-
-    weather.find({search: args[0], degreeType: args[1]}, (err, result) => {
-      if(err) {
-        console.log(err)
-        return
-      }
-
-      let toSend = "**Current Weather in "
-      + result[0].location.name + "**\`\`\`Temperature: "
-      + result[0].current.temperature + result[0].location.degreetype
-      + "\nFeels like: " + result[0].current.feelslike
-      + result[0].location.degreetype + "\nSky: "
-      + result[0].current.skytext + "\nWind: "
-      + result[0].current.winddisplay + "\`\`\`";
-
-      bot.createMessage(msg.channel.id, toSend)
-    })
   }
+
+  let command = args.join(' ')
+  let location = command.split('-d')[0].trim()
+  if (command.split('-d')[1] === 'undefined')
+    let degree = 'C'
+  else
+    let degree = command.split('-d')[1].trim()
+
+  weather.find({search: location, degreeType: args[1]}, (err, result) => {
+    if(err) {
+      console.log(err)
+      return
+    }
+
+    let toSend = "**Current Weather in "
+    + result[0].location.name + "**\`\`\`Temperature: "
+    + result[0].current.temperature + result[0].location.degreetype
+    + "\nFeels like: " + result[0].current.feelslike
+    + result[0].location.degreetype + "\nSky: "
+    + result[0].current.skytext + "\nWind: "
+    + result[0].current.winddisplay + "\`\`\`";
+
+    bot.createMessage(msg.channel.id, toSend)
+  })
 }
