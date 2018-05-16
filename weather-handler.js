8877524ce6f9eb800c32e585a320df8196347d2a
PlayerVM <lehmannic22@gamil.com>
2018-05-16 16:35:54 -0400

    Weather Embed

--- weather-handler.js
+++ weather-handler.js
@@ -1,5 +1,5 @@
 const weather = require('weather-js');
-
+const f = require('util').format
 
 exports.getWeather = (msg, args, bot) => {
   try {
@@ -11,24 +11,28 @@ exports.getWeather = (msg, args, bot) => {
     let command = args.join(' ')
     let location = command.split('-d')[0].trim()
 
-    let degree = 'C'
+    let degree = 'F'
     if (command.split('-d')[1] !== undefined) {
-      let degree = command.split('-d')[1].trim()
+      if (command.split('-d')[1].toUpperCase === 'C' || command.split('-d')[1].toUpperCase === 'F') {
+        let degree = command.split('-d')[1].trim()
+      }
     }
 
-    weather.find({search: location/*, degreeType: degree*/}, (err, result) => {
+    weather.find({search: location, degreeType: degree}, (err, result) => {
       if(err) {
-        console.log(err)
+        bot.createMessage(msg.channel.id, err)
         return
       }
 
-      let toSend = "**Current Weather in "
-      + result[0].location.name + "**\`\`\`Temperature: "
-      + result[0].current.temperature + result[0].location.degreetype
-      + "\nFeels like: " + result[0].current.feelslike
-      + result[0].location.degreetype + "\nSky: "
-      + result[0].current.skytext + "\nWind: "
-      + result[0].current.winddisplay + "\`\`\`";
+      let embed = {
+        embed: {
+            author: {name: f("**Current Weather in %s**", result[0].location.name), icon_url: result[0].current.imageUrl},
+            color: parseInt('0x4286f4', 16),
+            decription: f("Temperature: **%s**\nFeels like: **%s**\nSky: **%s**\nWind: **%s**\n", result[0].current.temperature + result[0].location.degreetype,
+              result[0].current.feelslike + result[0].location.degreetype, result[0].current.skytext, result[0].current.winddisplay),
+            footer: {text:'Part of the Broadcast Tower Integration Network'}
+        }
+      }
 
       bot.createMessage(msg.channel.id, toSend)
     })
