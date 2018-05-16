b1f3d133e57a52a374bcce7a3c69d769c3ef2088
PlayerVM <lehmannic22@gamil.com>
2018-05-16 15:55:22 -0400

    weather pls

--- weather-handler.js
+++ weather-handler.js
@@ -6,18 +6,15 @@ exports.getWeather = (msg, args, bot) => {
   if (args.length === 0) {
     bot.createMessage(msg.channel.id, 'please enter a location and degree type')
     return
-  } else if (args[1].toUpperCase() !== 'F' || args[1].toUpperCase() !== 'C') {
-    bot.createMessage(msg.channel.id, 'Degree type must be F or C you entered: ' + args[1])
-    return
-  }
+  } else if (args[1].toUpperCase() === 'F' || args[1].toUpperCase() === 'C') {
 
-  weather.find({search: args[0], degreeType: args[1]}, (err, result) => {
-    if(err) {
-      console.log(err)
-      return
-    }
+    weather.find({search: args[0], degreeType: args[1]}, (err, result) => {
+      if(err) {
+        console.log(err)
+        return
+      }
 
-    let toSend = "**Current Weather in "
+      let toSend = "**Current Weather in "
       + result[0].location.name + "**\`\`\`Temperature: "
       + result[0].current.temperature + result[0].location.degreetype
       + "\nFeels like: " + result[0].current.feelslike
@@ -25,6 +22,7 @@ exports.getWeather = (msg, args, bot) => {
       + result[0].current.skytext + "\nWind: "
       + result[0].current.winddisplay + "\`\`\`";
 
-    bot.createMessage(msg.channel.id, toSend)
-  })
+      bot.createMessage(msg.channel.id, toSend)
+    })
+  }
 }
