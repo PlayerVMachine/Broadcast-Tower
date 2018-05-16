09e135119f1162eb2e8409f9b11f61682be176dc
PlayerVM <lehmannic22@gamil.com>
2018-05-16 17:09:27 -0400

    weather forecast

--- weather-handler.js
+++ weather-handler.js
@@ -14,7 +14,7 @@ exports.getWeather = (msg, args, bot) => {
     let degree = 'F'
     if (command.split('-d')[1] !== undefined) {
       if (command.split('-d')[1].trim().toUpperCase() === 'C' || command.split('-d')[1].trim().toUpperCase() === 'F') {
-        degree = command.split('-d')[1].trim()
+        degree = command.split('-d')[1].trim().toUpperCase
       } else {
         degree ='F'
       }
@@ -30,7 +30,62 @@ exports.getWeather = (msg, args, bot) => {
         embed: {
             author: {name: f("Current Weather in %s", result[0].location.name), icon_url: result[0].current.imageUrl},
             color: parseInt('0x4286f4', 16),
-            description: f("Temperature: **%s**\nFeels like: **%s**\nSky: **%s**\nWind: **%s**\n", result[0].current.temperature + result[0].location.degreetype, result[0].current.feelslike + result[0].location.degreetype, result[0].current.skytext, result[0].current.winddisplay),
+            description: f("Temperature: **%s**\nFeels like: **%s**\nSky: **%s**\nWind: **%s**\n", result[0].current.temperature + degree, result[0].current.feelslike + degree, result[0].current.skytext, result[0].current.winddisplay),
+            footer: {text:'Part of the Broadcast Tower Integration Network'}
+        }
+      }
+
+      bot.createMessage(msg.channel.id, embed)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+exports.getForecast = (msg, args, bot) => {
+  try {
+    if (args.length === 0) {
+      bot.createMessage(msg.channel.id, 'please enter a location and degree type')
+      return
+    }
+
+    let command = args.join(' ')
+    let location = command.split('-d')[0].trim()
+
+    let degree = 'F'
+    if (command.split('-d')[1] !== undefined) {
+      if (command.split('-d')[1].trim().toUpperCase() === 'C' || command.split('-d')[1].trim().toUpperCase() === 'F') {
+        degree = command.split('-d')[1].trim()
+      } else {
+        degree ='F'
+      }
+    }
+
+    weather.find({search: location, degreeType: degree}, (err, result) => {
+      if(err) {
+        bot.createMessage(msg.channel.id, err)
+        return
+      }
+
+      let fields = []
+      for (i in result[0].forecast) {
+        if (result[0].forecast[i].percip === '')
+          var percip = '0%'
+        else
+          var pericp = result[0].forecast[i].percip + '%'
+
+        fields.push({name:result[0].forecast[i].day + f(' the %sth', result[0].forecast[i].Date.slice(8)),
+          value: f('High: **%s**\nLow: **%s**\nSky: **%s**\nPercipitation: **%s**',
+            result[0].forecast[i].high + degree, result[0].forecast[i].low + degree, result[0].forecast[i].skytextday, percip),
+          inline:true
+        })
+      }
+
+      let embed = {
+        embed: {
+            author: {name: f("Weather forecast in %s for the next 5 days", result[0].location.name), icon_url: result[0].current.imageUrl},
+            color: parseInt('0x4286f4', 16),
+            fields: fields,
             footer: {text:'Part of the Broadcast Tower Integration Network'}
         }
       }
