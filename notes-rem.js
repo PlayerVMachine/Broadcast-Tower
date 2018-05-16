056e0e72a6bdf39e7a5c0376d5cbdff4ffa07aa1
PlayerVM <lehmannic22@gamil.com>
2018-05-16 18:55:14 -0400

    get notes

--- notes-rem.js
+++ notes-rem.js
@@ -13,22 +13,54 @@ const authMechanism = 'DEFAULT'
 const url = f('mongodb://%s:%s@127.0.0.1:36505/broadcast_tower?authMechanism=%s', user, password, authMechanism)
 
 exports.noteToSelf = async (msg, args, bot) => {
-	let client = await MongoClient.connect(url)
-	const usersCol = client.db(config.db).collection('Users')
+	try{
+		let client = await MongoClient.connect(url)
+		const usersCol = client.db(config.db).collection('Users')
 
-	let usee = await usersCol.findOne({user: msg.author.id})
-	if (usee === null) {
-		bot.createMessage(msg.channel.id, f(reply.generic.useeNoAccount, msg.author.username))
-		return
-	}
+		let usee = await usersCol.findOne({user: msg.author.id})
+		if (usee === null) {
+			bot.createMessage(msg.channel.id, f(reply.generic.useeNoAccount, msg.author.username))
+			return
+		}
 
-	let toPin = await bot.createMessage(usee.sendTo, `Note: ` + args.join(' '), msg.attachments)
+		let toPin = await bot.createMessage(usee.sendTo, `Note: ` + args.push(msg.attachments).join(' '))
 
-	let pinned = await toPin.pin()
+		let pinned = await toPin.pin()
 
-	let msgToDel = await toPin.channel.getMessages(1,undefined, toPin.id)
+		let msgToDel = await toPin.channel.getMessages(1,undefined, toPin.id)
 
-	let deleted = await toPin.channel.deleteMessage(msgToDel[0].id)
+		let deleted = await toPin.channel.deleteMessage(msgToDel[0].id)
 
-	bot.createMessage(msg.channel.id, `Got it boss, note made!`)
+		bot.createMessage(msg.channel.id, `Got it boss, note made!`)
+	} catch (err) {
+		bot.createMessage(msg.channel.id, `Sorry boss my pencil broke`)
+	} 
 }
+
+exports.getNotes = async (msg, args, bot) => {
+	try{
+		let client = await MongoClient.connect(url)
+		const usersCol = client.db(config.db).collection('Users')
+
+		let usee = await usersCol.findOne({user: msg.author.id})
+		if (usee === null) {
+			bot.createMessage(msg.channel.id, f(reply.generic.useeNoAccount, msg.author.username))
+			return
+		}
+
+		let dmChannel = await bot.getDMChannel(msg.author.id)
+
+		let noteMsgs = await dmChannel.getPins()
+
+		let notes = []
+		for (m in noteMsgs) {
+			date = new Date(noteMsgs[m].timestamp)
+			notes.push(noteMsgs[m].content + f('Note made at: %s', date))
+		}
+
+		bot.createMessage(msg.channel.id, f(`%s your current notes are:\n%s`, msg.author.username, notes.join('\n')))
+
+	} catch (err) {
+		bot.createMessage(msg.channel.id, `Sorry boss my pencil broke`)
+	} 
+} 
