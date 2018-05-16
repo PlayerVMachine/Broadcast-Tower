e7ae9029f82c12cd0ef7f9c0bb078e249247693a
PlayerVM <lehmannic22@gamil.com>
2018-05-16 19:22:16 -0400

    attachments pls

--- notes-rem.js
+++ notes-rem.js
@@ -26,15 +26,11 @@ exports.noteToSelf = async (msg, args, bot) => {
 		let files = []
 		if (msg.attachments.length !== 0) {
 			for(i in msg.attachments) {
-				files.push(f(`[file %s](%s)`, i, msg.attachments.url))
+				files.push(f(`\n[file %s](%s)`, i, msg.attachments.url))
 			}
 		}
 
-		if (files.length === 0) {
-			let toPin = await bot.createMessage(usee.sendTo, f(`Note: %s`, args.join(' ')))
-		} else {
-			let toPin = await bot.createMessage(usee.sendTo, f(`Note: %s\n%s`, args.join(' '), files.join(' ')))
-		}
+		let toPin = await bot.createMessage(usee.sendTo, f(`Note: %s%s`, args.join(' '), files.join(' ')))
 
 		let pinned = await toPin.pin()
 
