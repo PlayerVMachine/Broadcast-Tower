36fbea88d4816f4ff371aaf4117f16d47d0d4508
PlayerVM <lehmannic22@gamil.com>
2018-05-17 17:30:17 -0400

    reminder goes into db

--- notes-rem.js
+++ notes-rem.js
@@ -120,66 +120,74 @@ exports.unNote = async (msg, args, bot) => {
 
 //function to connect to the db and add a reminder, need to parse time into date
 exports.remindMe = async (msg, args, bot) => {
+	try { 
+		let client = await MongoClient.connect(url)
+		const usersCol = client.db(config.db).collection('Users')
+		const remCol = client.db(config.db).collection('Reminders')
 
-	let client = await MongoClient.connect(url)
-	const usersCol = client.db(config.db).collection('Users')
-
-	let usee = await usersCol.findOne({user: msg.author.id})
-	if (usee === null) {
-		bot.createMessage(msg.channel.id, f(reply.generic.useeNoAccount, msg.author.username))
-		return
-	}
-
-	let format = new RegExp(/[0-9]*[yMdhm]{1,4}/)
-	let wrongM = new RegExp(/[0-9]*m(?=[0-9]*d)/)
-
-	let message = args.join(' ')
-
-	//parse message
-	let eom = message.lastIndexOf('in')
-	let reminder = message.slice(0, eom).trim()
-	let rawDate = message.slice(eom+2, message.length).trim().replace(/\s/g, '')
-
-	if (wrongM.test(rawDate)) {
-		bot.createMessage(msg.channel.id, 'Detected minutes before days!')
-	} else if (!format.test(rawDate)) {
-		bot.createMessage(msg.channel.id, 'Sorry I don\'t understand the format is yMdm')
-	} else {
-		let timeTypes = rawDate.split(/[0-9]*/).filter(val => val.length > 0)
-		let timeAmounts = rawDate.split(/[yMdhm]/).filter(val => val.length > 0)
-		let curDate = new Date()
-		let remYear = curDate.getFullYear()
-		let remMonth = curDate.getMonth()
-		let remDay = curDate.getDate()
-		let remHours = curDate.getHours()
-		let remMins = curDate.getMinutes()
-
-		if (timeTypes.indexOf('y') > -1)
-			remYear += parseInt(timeAmounts[timeTypes.indexOf('y')])
-
-		if (timeTypes.indexOf('M') > -1)
-			remMonth += parseInt(timeAmounts[timeTypes.indexOf('M')])
-
-		if (timeTypes.indexOf('d') > -1)
-			remDay += parseInt(timeAmounts[timeTypes.indexOf('d')])
-		
-		if (timeTypes.indexOf('h') > -1)
-			remHours += parseInt(timeAmounts[timeTypes.indexOf('h')])
-
-		if (timeTypes.indexOf('m') > -1)
-			remMins += parseInt(timeAmounts[timeTypes.indexOf('m')])
-
-		let remDate = new Date( remYear, remMonth, remDay, remHours, remMins)
-
-		let reminderObj = {
-			user: usee.user,
-			sendTo: usee.sendTo,
-			content: reminder,
-			due: remDate,
+		let usee = await usersCol.findOne({user: msg.author.id})
+		if (usee === null) {
+			bot.createMessage(msg.channel.id, f(reply.generic.useeNoAccount, msg.author.username))
+			return
 		}
 
-		bot.createMessage(msg.channel.id, 'Your reminder would be stored as: \n' + JSON.stringify(reminderObj))
-		
+		let format = new RegExp(/[0-9]*[yMdhm]{1,4}/)
+		let wrongM = new RegExp(/[0-9]*m(?=[0-9]*d)/)
+
+		let message = args.join(' ')
+
+		//parse message
+		let eom = message.lastIndexOf('in')
+		let reminder = `You wanted me to remind you: ` + message.slice(0, eom).trim()
+		let rawDate = message.slice(eom+2, message.length).trim().replace(/\s/g, '')
+
+		if (wrongM.test(rawDate)) {
+			bot.createMessage(msg.channel.id, 'Detected minutes before days!')
+		} else if (!format.test(rawDate)) {
+			bot.createMessage(msg.channel.id, 'Sorry I don\'t understand the format is yMdm')
+		} else {
+			let timeTypes = rawDate.split(/[0-9]*/).filter(val => val.length > 0)
+			let timeAmounts = rawDate.split(/[yMdhm]/).filter(val => val.length > 0)
+			let curDate = new Date()
+			let remYear = curDate.getFullYear()
+			let remMonth = curDate.getMonth()
+			let remDay = curDate.getDate()
+			let remHours = curDate.getHours()
+			let remMins = curDate.getMinutes()
+
+			if (timeTypes.indexOf('y') > -1)
+				remYear += parseInt(timeAmounts[timeTypes.indexOf('y')])
+
+			if (timeTypes.indexOf('M') > -1)
+				remMonth += parseInt(timeAmounts[timeTypes.indexOf('M')])
+
+			if (timeTypes.indexOf('d') > -1)
+				remDay += parseInt(timeAmounts[timeTypes.indexOf('d')])
+			
+			if (timeTypes.indexOf('h') > -1)
+				remHours += parseInt(timeAmounts[timeTypes.indexOf('h')])
+
+			if (timeTypes.indexOf('m') > -1)
+				remMins += parseInt(timeAmounts[timeTypes.indexOf('m')])
+
+			let remDate = new Date( remYear, remMonth, remDay, remHours, remMins)
+
+			let reminderObj = {
+				user: usee.user,
+				sendTo: usee.sendTo,
+				content: reminder,
+				due: remDate,
+			}
+
+			let addRem = await remCol.insertOne(reminderObj)
+			if (addRem.insertedCount === 1)
+				bot.createMessage(msg.channel.id, f('Got it I\'ll remind you: %s in %s', message.slice(0, eom).trim(), rawDate))
+			else
+				bot.createMessage(msg.channel.id, 'Uh oh! I can\'t remember that for you right now!')
+			
+		}
+	} catch (err) {
+		console.log(err)
 	}
 }
 
