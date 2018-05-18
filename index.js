41742c86d2e4f0e15ef74abe62bf46f36fd0774e
PlayerVM <lehmannic22@gamil.com>
2018-05-18 12:56:20 -0400

    add color to hello

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ const raven = bot.registerCommand('Night', (msg, args) => {
 
 const hello = bot.registerCommand('hello', (msg, args) => {
 	bot.createMessage(msg.channel.id, {embed: {
-					color: config.eColors,
+					color: config.eColor,
 					description:f(reply.generic.hello, msg.author.username)
 				}})
 })
