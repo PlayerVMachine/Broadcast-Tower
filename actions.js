36c1010ba6458ce52c6068b52e1fde5f97fb61b3
Nicolas Lehman <lehmannic22@gmail.com>
2018-05-20 21:12:36 -0400

    needed to pass q

--- actions.js
+++ actions.js
@@ -335,7 +335,7 @@ exports.post = async (msg, args, bot, q, client) => {
 	}
 }
 
-exports.reply = async (msg, args, bot, client) => {
+exports.reply = async (msg, args, bot, q, client) => {
 	try {
 		const col = client.db(config.db).collection('Users')
 		let usee = await col.findOne({user: msg.author.id})
