38b37b1757c8de3875d84c94748a1d19eb3934a9
Nicolas Lehman <lehmannic22@gmail.com>
2018-05-23 16:51:09 -0400

    redefine degree

--- weather-handler.js
+++ weather-handler.js
@@ -178,7 +178,10 @@ exports.dailyForecast = async (destination, client, q, bot) => {
     const col = client.db(config.db).collection('Users')
     let usee = await col.findOne({sendTo: destination})
 
-    weather.find({search: usee.weather.location, degreeType: usee.weather.deg}, (err, result) => {
+    let location = usee.weather.location
+    let degree = usee.weather.deg
+
+    weather.find({search: location, degreeType: degree}, (err, result) => {
       if(err) {
         bot.createMessage(config.logChannelID, err)
         return
