cf8e17fd6c17245ac875bd592be7ad011ffc735d
Nicolas Lehman <lehmannic22@gmail.com>
2018-05-24 19:50:58 -0400

    use Date.parse() properly

--- weather-handler.js
+++ weather-handler.js
@@ -159,7 +159,7 @@ exports.dailySub = async (msg, args, bot, client) => {
     if (scheduledTime.getDate() > now.getDate())
       scheduledTime = new Date(Date.parse(scheduledTime) - 24*60*60*1000)
 
-    if (scheduledTime.parse() < now.parse())
+    if (Date.parse(scheduledTime) < Date.parse(now))
        scheduledTime = new Date(Date.parse(scheduledTime) + 24*60*60*1000)
 
     let weatherSub = {
